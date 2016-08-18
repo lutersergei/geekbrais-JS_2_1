dc1883e3a8dd8eaa422eba930074fcc9ba0eefa7
Luter <lutersergei@mail.ru>
2016-08-18 15:25:48 +0700

    опции установить минимум и максимум

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function CounterField(selector) {
     var input = document.createElement('input');
     this.incrementStep = 1;
     this.decrementStep = 1;
+    this.minValue = undefined;
+    this.maxValue = undefined;
     input.onkeydown = function (event)
     {
         if (((event.keyCode<48)||(event.keyCode>57)) && (event.keyCode !== 8) && ((event.keyCode<96)||(event.keyCode>105)))
@@ -25,9 +27,35 @@ function CounterField(selector) {
     var self = this;
     function increment () {
         input.value = +input.value + self.incrementStep;
+        input.value = checkValue(input.value);
     }
     function decrement () {
         input.value = +input.value - self.decrementStep;
+        input.value = checkValue(input.value);
+    }
+    function checkValue(value) {
+        // console.log(self.minValue , self.maxValue);
+        if ((self.minValue === undefined) && (self.maxValue === undefined))
+        {
+            // console.log('undefined');
+            return value;
+        }
+        //Проверка на правильность введеный минимум и максимум значений
+        if (self.minValue >= self.maxValue)
+        {
+            return value;
+        }
+        if (self.minValue !== undefined)
+        {
+            // console.log('min');
+            if (value < self.minValue) value = self.minValue;
+        }
+        if (self.maxValue !== undefined)
+        {
+            // console.log('max');
+            if (value > self.maxValue) value = self.maxValue;
+        }
+        return value;
     }
 }
 
@@ -36,3 +64,5 @@ var counterfield_1 = new CounterField('#counterfield_1');
 var counterfield_2 = new CounterField('#counterfield_2');
 counterfield_2.incrementStep = 10;
 counterfield_2.decrementStep = 10;
+counterfield_2.minValue = 0;
+counterfield_2.maxValue = 60;
